d8676878b3458290a2987f8833441f4b51ebf08c
Oussema Hakim <41113028+HakOuss@users.noreply.github.com>
2022-12-06 22:25:14 +0100

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -212,7 +212,7 @@ ServerRequest::addDetector('tablet', function ($request) {
 
 // There is no time-specific type in Cake
 TypeFactory::map('time', StringType::class);
-header('Access-Control-Allow-Origin: *']);
+header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: POST, GET, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: *');
